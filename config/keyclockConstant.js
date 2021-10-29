59310b8cbf4e25d9b12e5a479d309ad7af4f4a09
hatim daudi <hatimtechnologies@gmail.com>
2021-10-29 14:13:32 +0530

    added contstant file in edit

--- config/keyclockConstant.js
+++ config/keyclockConstant.js
@@ -11,16 +11,16 @@ module.exports.GROUPS = {
     BANK_BRANCH_GROUP: "Bank Branch"
 }
 
-module.exports.REALM_NAME = "Ewallet"
+module.exports.REALM_NAME = "ewallet"
 
 module.exports.KEYCLOCK_IP = "http://localhost:8080/auth"
 
 module.exports.CLIENT_ID = "infra-client"
 
-module.exports.ID_OF_CLIENT = "cc6b0966-a696-4c19-811e-2ae115ee8cba"
+module.exports.ID_OF_CLIENT = "9cb3cd66-4537-4539-ada5-026e10ea152f"
 
 module.exports.ADMIN_USERNAME = "admin"
 
 module.exports.ADMIN_PASSWORD = "admin"
 
-module.exports.CLIENT_SECRET = "a37922ad-9d46-448d-9957-0e30db83b4d8"
+module.exports.CLIENT_SECRET = "764e649b-3912-4187-bd3e-1531b3fedd7d"
